e9e7c386fc4a5eb93f33cbf82cdac12710a644cb
filipeamorais@pm.me <filipeamorais@pm.me>
2019-07-31 21:47:24 -0500

    add: create new route for the page add: new table for produtos com desconto add: new instance variable for the new table

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,6 +1,7 @@
 class ProdutosController < ApplicationController
 
     def index
-        @produtos = Produto.order : nome
+        @produtos = Produto.order(nome: :desc).limit 2
+        @produtocomdesconto = Produto.order(:preco).limit 1
     end
 end
